2c9e04513ccf231d6b7e2284fbbd3106cc4ea550
Jacqui Kane <tjrkane@gmail.com>
2025-01-22 20:28:30 -0500

    Update to defs in mod2

--- module2/dictionary.js
+++ module2/dictionary.js
@@ -4,35 +4,42 @@ const dictionary = {
   "High Level Language, HLL":
     "Programming Language, <i>readable</i>, because contains words like <i>while, if, else</i>",
   "Low Level Language":
-    "Language that is closer to the processor, like assembly language, machine language",
+    "Language that is closer to the processor, like assembly language, machine language. <br>MOV 001, <br>10000001 11",
   Software:
-    "A set of instructions used to operate computing devices and perform tasks",
-  Hardware: "Computer's tangible components",
-  Applications: "Computer software performing specific tasks for end-user",
-  CPU: "Central Processing Unit",
+    "Set of instructions used to operate computing devices and perform tasks. Created with programming languages, High Level Languages, Low Level Languages.",
+  Hardware:
+    "Computer's tangible components - like the disk, the processor, the motherboard.",
+  Applications:
+    "Computer software performing specific tasks for end-user, created with a programming language.",
+  CPU: "Central Processing Unit - the main chip or processor in the computer system, like the Intel Core i9 processor",
   "Primary Storage":
-    "Computer Memory, RAM. This is volatile storage, clears when no electricity supply!",
-  "Secondary Storage": "Permanent storage, like on disk",
-  HDD: "Hard Disk Drive, older technology that stores data on spinning magnetic discs",
-  SSD: "Solid State Drive (or Device), uses flash menory to store data",
-  Input: "Process of passing data to a computer application",
-  Output: "Process of displaying results of computation",
+    "Computer Memory, RAM. This is volatile storage, clears when there is no electricity supply!",
+  "Secondary Storage":
+    "Permanent storage, like on disk - could be CD, flash drive, HDD, SSD, even floppy disc!",
+  HDD: "Hard Disk Drive, older technology that stores data on spinning magnetic discs. Data can be erased if disc head crashes.",
+  SSD: "Solid State Drive (or Device), newer than HDD, and uses flash memory to store data.",
+  Input:
+    "Process of passing data to a computer application by some mechanism not limited to keyboard, touch sensitive screen, mouse, microphone.",
+  Output:
+    "Process of displaying or rendering results of computation - like display on a monitor or audio through speakers.",
   Interpreter:
-    "Software that renders program code to machine language, statement by statement",
+    "Software that renders program code to machine language, statement by statement. Python is an interpreted language.",
   Compiler:
-    "Software that renders all program code in a program,  to machine language at the one time",
+    "Software that renders all program code in a program,  to machine language at the one time. Typically results in an executable. C is a compiled language.",
   Comment:
-    "Annotation that is igniored by compiler or interpreter, like single line comment, #, in Python",
+    "Annotation that is ignored by compiler or interpreter, like single line comment, #, in Python.",
   "Camel Casing":
-    "Naming convention - use lower case first letter, and upper case first letter for each subsequent word in name - like studentGradYear",
+    "Naming convention - use lower case first letter, and upper case first letter for each subsequent word in name - like studentGradYear, productData.",
   "Snake Casing":
-    "Naming convention, use all lower case, separating each logical word with an underscore, like student_grad_year",
+    "Naming convention, use all lower case, separating each logical word with an underscore, like student_grad_year, product_data.",
   "Pascal Casing":
-    "Naming convention, use upper case first letter, and upper case first letter for all subsequent logocal words in anme, like StudentGradYear",
+    "Naming convention, use upper case first letter, and upper case first letter for all subsequent logocal words in anme, like StudentGradYear, ProductData.",
   "String Literal":
-    'String that has not been assigned to a variable, but is used in program code, like print("Hi there")',
-  Boolean: "Data type, value of True or False only",
-  String: 'Data type, uses text encased in quotes, like "Hi there"',
+    'String that has not been assigned to a variable, but is used in program code, like print("Hi there"). In this case, the string "Hi there" would be a string literal.',
+  Boolean:
+    "Data type, value of True or False only. Extensively using in computer programming to detrmine branching and store results of Boolean Logic operations.",
+  String:
+    'Data type, uses text encased in quotes, like "Hi there". String data type is used to handle text data.',
   "float, Floating Point":
     "Data type, numeric, supports numbers after decimal point - like 1.23",
   "int, Integer": "Data type, supports whole numbers, no decimal point, like 5",
@@ -40,23 +47,26 @@ const dictionary = {
     "Diagram that illustrates variables and associated values",
   Variable: "Named location in computer memory",
   "Data Type":
-    "Caretory of data that a quantity belongs to, like Integer, Boolean",
-  "Data Value": "Quantity stored in a variable",
-  Function: "Block of code that is given a name by which to call it",
-  IDE: "Integrated Development Environment, tool for creating , managing and executing code, like VS Code",
-  GUI: "Graphical User Interface, user interface that includes visual elements like windows, buttons, menu bars",
+    "Category of data that a quantity belongs to, like Integer, Boolean, String, Float",
+  "Data Value":
+    "Quantity stored in a variable, like<br>studentGPA = 3.8<br>... the data value of the variable, studentGPA, is 3.8",
+  Function:
+    "Block of code that is given a name by which to call it, like the special main function in Python.",
+  IDE: "Integrated Development Environment, tool for creating , managing and executing code, like VS Code, Brackets, PyCharm.",
+  GUI: "Graphical User Interface, user interface that includes visual elements like windows, buttons, menu bars.",
   Indentation:
-    "Typically the use of a tab character, and used to indocation containment or code taht is within the body of a block, like code indented within a function in Python",
-  Indent: "Adding an indentation",
-  Dedent: "Removing an indentation",
+    "Typically the use of a tab character, and used to indicate containment or code within the body of a block, like code indented within a function in Python",
+  Indent: "Adding an indentation to a code block to indicate containment.",
+  Dedent:
+    "Removing an indentation from a code block to change the scope of the code, perhaps remove the code from a function.",
   Docstring:
     'Multi-line comment, """ .... """ at the top of a Python file, includes information like name of developer, date created',
   "Escape Sequences":
-    'Special characters that embedded in strings and are intertreted as new line, tabs, like print("This is a \\n Newline")',
+    'Special characters that are embedded in strings and are intertreted as new line, tabs, like print("This is a \\n Newline").',
   "Conversion Function":
-    "Function that converts the data type of a variable, to another data type, like converting int variable, studentGradYear, to a String, with str(sudentGradYear)",
+    "Function that converts the data type of a variable, to another data type, like converting int variable, studentGradYear, to a String, with str(sudentGradYear).",
   "Strongly Typed":
-    "Enforcement of strict rules for mixing data types and variables",
+    "Enforcement of strict rules for mixing data types and variables. Python is stronlly typed.",
   "Dynamically Typed":
-    "Data type of variable is determined at runtime, after an assignment - like Python",
+    "Data type of variable is determined at runtime, after an assignment. Python is dynamically typed.",
 };
