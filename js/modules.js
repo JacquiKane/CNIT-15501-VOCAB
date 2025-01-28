89058a40bc911b21439daac7ca10c4f0aaa70477
Jacqui Kane <tjrkane@gmail.com>
2025-01-27 21:47:29 -0500

    Update for module3, unit 2

---
+++ js/modules.js
@@ -0,0 +1,20 @@
+/** @format */
+
+$(document).ready(function () {
+  const items = [
+    { title: "Week 2, Data Types & Variables", link: ".\\module2\\index.html" },
+    {
+      title: "Week 3, Math Ops and Conditionals",
+      link: ".\\module3\\index.html#",
+    },
+    { title: "Week 4", link: "#" },
+  ];
+
+  items.forEach((item) => {
+    const card = $('<div class="card"></div>').text(item.title);
+    card.on("click", function () {
+      window.location.href = item.link;
+    });
+    $("#card-container").append(card);
+  });
+});
